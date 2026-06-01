da43fefbc5f0e2c1c8a364c1f0a8ae247cfde5a0
jiangood <410518072@qq.com>
2026-06-01 16:32:26 +0800

    feat: add EntityScan annotation to ProcessBootApplication

--- open-admin-flowable-example/src/main/java/io/github/jiangood/openadmin/ProcessBootApplication.java
+++ open-admin-flowable-example/src/main/java/io/github/jiangood/openadmin/ProcessBootApplication.java
@@ -2,8 +2,10 @@ package io.github.jiangood.openadmin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.persistence.autoconfigure.EntityScan;
 
 @SpringBootApplication
+@EntityScan(basePackageClasses = ProcessBootApplication.class)
 public class ProcessBootApplication {
 
     public static void main(String[] args) {

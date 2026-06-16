5796eaef6ecbdbf27a3917fd74ba17a33accec31
jiangood <410518072@qq.com>
2026-06-16 14:09:49 +0800

    chore: bump version to 2.1.0
    - Upgrade Spring Boot 4.0.6→4.1.0, open-admin 2.2.8→2.4.1
    - Upgrade all npm dependencies, remove jsencrypt
    - Fix tests and H2 compatibility for new dependency versions

--- open-admin-flowable-starter/src/test/java/io/github/jiangood/openadmin/modules/flowable/utils/ModelToolTest.java
+++ open-admin-flowable-starter/src/test/java/io/github/jiangood/openadmin/modules/flowable/utils/ModelToolTest.java
@@ -2,6 +2,7 @@ package io.github.jiangood.openadmin.modules.flowable.utils;
 
 import io.github.jiangood.openadmin.modules.flowable.common.utils.ModelTool;
 import org.flowable.bpmn.model.BpmnModel;
+import org.flowable.bpmn.model.UserTask;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -10,7 +11,7 @@ class ModelToolTest {
     @Test
     void testXmlToModel_roundtrip() {
         String xml = """
-                <?xml version="1.0" encoding="UTF-8"?>
+<?xml version="1.0" encoding="UTF-8"?>
 <definitions xmlns="http://www.omg.org/spec/BPMN/20100524/MODEL"
              xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
              targetNamespace="http://flowable.org/bpmn">
@@ -31,11 +32,15 @@ class ModelToolTest {
     }
 
     @Test
-    void testValidateModel_emptyModel_shouldFail() {
+    void testValidateModel_taskWithoutAssignee_shouldFail() {
         BpmnModel model = new BpmnModel();
         org.flowable.bpmn.model.Process process = new org.flowable.bpmn.model.Process();
-        process.setId("emptyProcess");
+        process.setId("testProcess");
         process.setExecutable(true);
+        UserTask task = new UserTask();
+        task.setId("userTask1");
+        task.setName("Test Task");
+        process.addFlowElement(task);
         model.addProcess(process);
 
         assertThrows(IllegalArgumentException.class, () -> ModelTool.validateModel(model));

ddbdf0ed9161dbb62718de27fed83598f1252726
jiangood <410518072@qq.com>
2026-06-17 17:39:33 +0800

    fix: adjust column width and title for task table

--- web/src/pages/flowable/user-task/index.jsx
+++ web/src/pages/flowable/user-task/index.jsx
@@ -6,10 +6,10 @@ function TodoTable({onProcess}) {
     const columns = [
         {title: '发起人', dataIndex: 'instanceStarter'},
         {title: '流程名称', dataIndex: 'instanceName'},
-        {title: '当前节点', dataIndex: 'taskName', width: 100},
+        {title: '当前节点', dataIndex: 'taskName', width: 150},
         {title: '当前操作人', dataIndex: 'assigneeInfo', width: 100},
         {title: '发起时间', dataIndex: 'instanceStartTime'},
-        {title: '任务创建时间', dataIndex: 'createTime'},
+        {title: '创建时间', dataIndex: 'createTime'},
         {
             title: '操作', dataIndex: 'option',
             render: (_, record) => (

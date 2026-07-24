2ab8d50f7916f5091132a23d45aa60deea01bbef
jiangood <410518072@qq.com>
2026-07-24 13:50:40 +0800

    chore: upgrade open-admin to 2.5.9, bump version to 2.5.10

--- open-admin-flowable/src/main/java/io/github/jiangood/openadmin/modules/flowable/config/FlowableProcessInitializer.java
+++ open-admin-flowable/src/main/java/io/github/jiangood/openadmin/modules/flowable/config/FlowableProcessInitializer.java
@@ -4,23 +4,33 @@ import io.github.jiangood.openadmin.framework.config.StartupHook;
 import io.github.jiangood.openadmin.modules.flowable.domain.ProcessMeta;
 import io.github.jiangood.openadmin.modules.flowable.service.ProcessMetaService;
 import io.github.jiangood.openadmin.modules.flowable.service.ProcessModelService;
-import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
 
 @Slf4j
 @Component
-@AllArgsConstructor
 public class FlowableProcessInitializer implements StartupHook {
 
     private ProcessMetaService processMetaService;
 
     private ProcessModelService processModelService;
 
+    @Autowired
+    public void setProcessMetaService(@Lazy ProcessMetaService processMetaService) {
+        this.processMetaService = processMetaService;
+    }
+
+    @Autowired
+    public void setProcessModelService(@Lazy ProcessModelService processModelService) {
+        this.processModelService = processModelService;
+    }
+
     @Override
-    public void beforeSystemDataInitialize() {
+    public void beforeSeedDataInitialize() {
         log.info("===== FlowableProcessInitializer 开始执行 =====");
         List<ProcessMeta> list = processMetaService.findAll();
         log.info("===== 流程定义数量: {} =====", list.size());
